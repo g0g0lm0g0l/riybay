bfcd0e40181e3538c512970afa078662945ca0b8
ikovalenko <ikovalenko@grupocimd.com>
2024-06-05 13:16:09 +0200

    Añadido un triangulo, se puede arrastrar hacía abajo haciendo el triangulo más grando por los x's

--- raybay.py
+++ raybay.py
@@ -1,8 +1,8 @@
 from raylib import *
 
-HIGHT = 1200
-WIDTH = 800 
-    
+HIGHT=1200
+WIDTH=800 
+
 def main():
     InitWindow(HIGHT, WIDTH, b"Asteroids")
     SetTargetFPS(60)
@@ -26,11 +26,15 @@ def main():
         if IsKeyPressed(KEY_SPACE):
             for i in range(posY_rect, 0, -1):
                 DrawRectangleGradientH(int(posX_rect+WIDTH/10/4), i, int(WIDTH/20), int(HIGHT/20), RED, GREEN)
-
+        i += 1
+        DrawTriangle([posX_rect-WIDTH/3+int(WIDTH/20), HIGHT/3], [posX_rect+int(WIDTH/20), posY_rect], [posX_rect+WIDTH/3, HIGHT/3], MAGENTA)
+        DrawPoly([int(WIDTH/4+500)-GetFPS(), int(HIGHT/4)-GetFPS()], 10, 50.0+i, 0.0, WHITE)
         DrawText((str(GetFPS())).encode("ascii"), 10, 10, 20, SKYBLUE)
+        DrawCircleGradient(int(WIDTH/3)+GetFPS(), int(HIGHT/3)-GetFPS(), 100.0+i, RED, YELLOW)
+        DrawEllipse(int(WIDTH/5)+GetFPS(), int(HIGHT/5+300)+GetFPS(), 40.0, 60.0+i, VIOLET)
         DrawRectangleGradientH(posX_rect, posY_rect, int(WIDTH/10), int(HIGHT/10), YELLOW, GREEN)
-        DrawCircleGradient(int(WIDTH/3)+GetFPS(), int(HIGHT/3)-GetFPS(), 100.0, BLUE, RED)
-        DrawEllipse(int(WIDTH/4)+GetFPS(), int(HIGHT/4)+GetFPS(), 30.0, 60.0, SKYBLUE)
+        if i == 100:
+            i = 0
         EndDrawing()
 
 
